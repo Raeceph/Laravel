6cd7b3d0ead77c302d4f7f829664ef2ca2ce6303
Raeceph <cephy88@gmail.com>
2017-01-02 18:57:57 +0800

    final

--- routes/web.php
+++ routes/web.php
@@ -11,28 +11,54 @@
 |
 */
 
-
-Route::group(['middleware' => ['web']],function(){
-	Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
+})->name('home');
+Route::post('/signup',[
+     'uses' => 'UserController@postSignUp',
+	 'as' => 'signup'
+]);
+Route::post('/signin',[
+     'uses' => 'UserController@postSignIn',
+	 'as' => 'signin'
+]);
+Route::post('/createpost',[
+     'uses' => 'PostController@postCreatePost',
+	 'as' => 'post.create'
+]);
+Route::get('/logout', [
+     'uses' => 'UserController@getLogout',
+	 'as' => 'logout'
+]);
+Route::post('/edit', [
+    'uses' => 'PostController@postEditPost',
+    'as' => 'edit'
+]);
+Route::get('/account', [
+    'uses' => 'UserController@getAccount',
+    'as' => 'account'
+]);
+Route::post('/upateaccount', [
+    'uses' => 'UserController@postSaveAccount',
+    'as' => 'account.save'
+]);
+Route::get('/userimage/{filename}', [
+    'uses' => 'UserController@getUserImage',
+    'as' => 'account.image'
+]);
+Route::post('/like', [
+    'uses' => 'PostController@postLikePost',
+    'as' => 'like'
+]);
+Route::group(['middleware' => 'auth'], function () {
+Route::get('/dashboard', [
+     'uses' => 'PostController@getDashboard',
+	 'as' => 'dashboard'
+]);
+Route::get('/delete-post/{post_id}', [
+     'uses' => 'PostController@getDeletePost',
+	 'as' => 'post.delete'
+]);
 });
 
-	
-	Route::post('/signup', [
-	'uses' => 'UserController@postSignUp',
-	'as' => 'signup'
-]);
 
-	Route::post('/signin', [
-	'uses' => 'UserController@postSignIn',
-	'as' => 'signIn'
-]);
-	
-	Route::get('/dashboard',[
-	'uses' => 'UserController@getDashboard',
-	'as' => 'dashboard'
-]);
-	
-	
-	
-});
